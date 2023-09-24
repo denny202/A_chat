7a243b15fd6ff05a13040450bef5941e4ba272ea
denny202 <iomassimetto@gmail.com>
2023-09-24 18:28:06 +0100

    - added templates - added views for A_room - added index view

--- A_chatroom/views.py
+++ A_chatroom/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index (request):
+    return render(request,'iallrooms.html')
+
+
+def A_room(request,name):
+    return render (request,'room.html',{'name':name})
